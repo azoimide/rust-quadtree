4366903a7feae67c3e6d801f5a3438e13341f094
niklasf <niklasf@cs.umu.se>
2016-06-17 23:50:18 +0200

    hard-coded span type dir_of done

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,14 @@
 mod quadtree;
 
-use quadtree::QuadTree;
+use quadtree::{QuadTree, Position, PosSpan};
 
 fn main() {
 
-    let mut tree: QuadTree<Position> = QuadTree::new();
+    let mut tree = QuadTree::new(PosSpan {
+        nw: Position { x: 0, y: 0 },
+        width: 10,
+        height: 10
+    });
     tree.visit();
     tree.add(Position { x: 1, y: 2 });
     tree.add(Position { x: 1, y: 2 });
@@ -17,8 +21,9 @@ fn main() {
 
 }
 
-#[derive(Debug)]
-struct Position {
-    x: i32,
-    y: i32
-}
+
+// impl<Position> Span<Position> for PosSpan {
+//     fn dir_of(&self, t: &Position) -> Option<Dir> {
+//         return self.dir_of(t);
+//     }
+// }
--- src/quadtree.rs
+++ src/quadtree.rs
@@ -68,7 +68,7 @@ impl<T: Debug> Node<T> {
 }
 
 impl<T: Debug> QuadTree<T> {
-    pub fn new() -> QuadTree<T> {
+    pub fn new(span: PosSpan) -> QuadTree<T> {
         return QuadTree { root: Node::new() };
     }
 
@@ -81,6 +81,64 @@ impl<T: Debug> QuadTree<T> {
         self.root.add(t);
     }
 }
+
+pub enum Dir { N, S, E, W, NE, NW, SE, SW }
+
+#[derive(Debug, PartialEq, Eq)]
+pub struct Position {
+    pub x: i32,
+    pub y: i32
+}
+
+#[derive(Debug, PartialEq, Eq)]
+pub struct PosSpan {
+    pub nw: Position,
+    pub width: i32,
+    pub height: i32
+}
+
+impl PosSpan {
+// impl<Position> Span<Position> for PosSpan {
+    fn dir_of(&self, t: &Position) -> Option<Dir> {
+        if t.x < self.nw.x {
+            if t.y < self.nw.y {
+                return Some(Dir::NW);
+            } else if t.y < self.nw.y + self.height {
+                return Some(Dir::W);
+            } else {
+                return Some(Dir::SW);
+            }
+        } else if t.x < self.nw.x + self.width {
+            if t.y < self.nw.y {
+                return Some(Dir::N);
+            } else if t.y < self.nw.y + self.height {
+                return None;
+            } else {
+                return Some(Dir::S);
+            }
+        } else {
+            if t.y < self.nw.y {
+                return Some(Dir::NE);
+            } else if t.y < self.nw.y + self.height {
+                return Some(Dir::E);
+            } else {
+                return Some(Dir::SE);
+            }
+        }
+    }
+
+    // fn split(self) -> Vec<PosSpan> {
+
+    // }
+}
+
+// pub trait Span<T> {
+//     fn dir_of(&self, t: &T) -> Option<Dir>;
+//     // fn north_span(&self) -> Span<T>;
+//     // fn south_span(&self) -> Span<T>;
+//     // fn east_span(&self) -> Span<T>;
+//     // fn west_span(&self) -> Span<T>;
+// }
 
 
 
