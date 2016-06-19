f1e80cdae4777dd0af9dce81cba32c6977917870
niklasf <niklasf@cs.umu.se>
2016-06-19 14:51:54 +0200

    refactor

--- src/quadtree.rs
+++ src/quadtree.rs
@@ -13,23 +13,47 @@ pub struct QuadTree<S, T: Debug> {
     root: Node<S, T>
 }
 
-#[derive(Debug)]
-enum Child<S, T: Sized + Debug> {
-    Inner(Box<Node<S, T>>),
-    Leaf(S, Vec<T>)
-}
+impl<S: Span<S, T> + Debug, T: Debug + Clone + PartialEq> QuadTree<S, T> {
+    pub fn new(span: S) -> QuadTree<S, T> {
+        return QuadTree { root: Node::new(span) };
+    }
 
-impl<S, T: Debug> Child<S, T> {
-    // fn visit(&self) {
-    //     match self {
-    //         &Child::Leaf(_, ref t) => {
-    //             println!("{:?}", t);
-    //         },
-    //         &Child::Inner(ref b) => {
-    //             b.as_ref().visit();
-    //         }
-    //     }
-    // }
+    pub fn add(&mut self, t: T) {
+        self.ensure_contains(&t);
+        self.root.add(t);
+    }
+
+    pub fn print(&self) {
+        print!("root: ");
+        self.root.print(1);
+    }
+
+    fn ensure_contains(&mut self, t: &T) {
+        while !self.root.span.contains(&t) {
+            let new_span = self.root.span.expand(&self.root.span.dir_of(&t).unwrap());
+            let mut new_root = Node::new(new_span);
+            for (dir, span) in new_root.span.split() {
+                if span == self.root.span {
+                    mem::swap(&mut self.root, &mut new_root);
+                    self.root.children.insert(dir.clone(), Child::Inner(Box::new(new_root)));
+                    break;
+                }
+                // unreachable!();
+            }
+        }
+    }
+
+    pub fn scan(&self, span: &S) -> Vec<T> {
+        return self.root.scan(span);
+    }
+
+    pub fn contains(&self, t: &T) -> bool {
+        return self.root.contains(t);
+    }
+
+    pub fn remove(&self, t: &T) -> bool {
+        unimplemented!();
+    }
 }
 
 #[derive(Debug)]
@@ -47,48 +71,44 @@ impl<S: Span<S, T> + Debug, T: Debug + Clone + PartialEq> Node<S, T> {
         };
     }
 
-    // fn visit(&self) {
-    //     // for i in 0..4 {
-    //     //     match self.children.get(i) {
-    //     //         Some(c) => c.visit(),
-    //     //         None => println!("None")
-    //     //     };
-    //     // };
-    // }
-
     fn add(&mut self, t: T) {
         let mut sub_spans = self.span.split();
         for (key, span) in sub_spans.drain() {
             if span.contains(&t) {
-                if self.children.contains_key(&key) {
-                    let child = self.children.remove(&key).unwrap();
-                    let new_child: Child<S, T> = match child {
-                        Child::Leaf(span, mut v) => {
-                            if v.len() > 1 && self.span.can_split() {
-                                let mut new_node = Node::new(span);
-                                for old_t in v.drain(RangeFull) {
-                                    new_node.add(old_t);
-                                }
-                                new_node.add(t);
-                                Child::Inner(Box::new(new_node))
-                            } else {
-                                v.push(t);
-                                Child::Leaf(span, v)
-                            }
-                        },
-                        Child::Inner(mut b) => {
-                            b.as_mut().add(t);
-                            Child::Inner(b)
-                        }
-                    };
-                    self.children.insert(key.clone(), new_child);
-                } else {
-                    self.children.insert(key.clone(), Child::Leaf(span, vec![t]));
-                }
+                let old_child = self.children.remove(&key);
+                let new_child = self.new_child(span, old_child, t);
+                self.children.insert(key, new_child);
                 return;
             }
         }
         unreachable!("Tried to add {:?} to node with span {:?}", t, self.span);
+    }
+
+    fn new_child(&self, span: S, child: Option<Child<S, T>>, t: T) -> Child<S, T> {
+        return match child {
+            None => Child::Leaf(span, vec![t]),
+            Some(c) => {
+                match c {
+                    Child::Leaf(span, mut v) => {
+                        if v.len() > 1 && self.span.can_split() {
+                            let mut new_node = Node::new(span);
+                            for old_t in v.drain(RangeFull) {
+                                new_node.add(old_t);
+                            }
+                            new_node.add(t);
+                            Child::Inner(Box::new(new_node))
+                        } else {
+                            v.push(t);
+                            Child::Leaf(span, v)
+                        }
+                    },
+                    Child::Inner(mut b) => {
+                        b.as_mut().add(t);
+                        Child::Inner(b)
+                    }
+                }
+            }
+        };
     }
 
     fn print(&self, depth: usize) {
@@ -147,47 +167,10 @@ impl<S: Span<S, T> + Debug, T: Debug + Clone + PartialEq> Node<S, T> {
     }
 }
 
-impl<S: Span<S, T> + Debug, T: Debug + Clone + PartialEq> QuadTree<S, T> {
-    pub fn new(span: S) -> QuadTree<S, T> {
-        return QuadTree { root: Node::new(span) };
-    }
-
-    pub fn add(&mut self, t: T) {
-        self.ensure_contains(&t);
-        self.root.add(t);
-    }
-
-    pub fn print(&self) {
-        print!("root: ");
-        self.root.print(1);
-    }
-
-    fn ensure_contains(&mut self, t: &T) {
-        while !self.root.span.contains(&t) {
-            let new_span = self.root.span.expand(&self.root.span.dir_of(&t).unwrap());
-            let mut new_root = Node::new(new_span);
-            for (dir, span) in new_root.span.split() {
-                if span == self.root.span {
-                    mem::swap(&mut self.root, &mut new_root);
-                    self.root.children.insert(dir.clone(), Child::Inner(Box::new(new_root)));
-                    break;
-                }
-                // unreachable!();
-            }
-        }
-    }
-
-    pub fn scan(&self, span: &S) -> Vec<T> {
-        return self.root.scan(span);
-    }
-
-    pub fn contains(&self, t: &T) -> bool {
-        return self.root.contains(t);
-    }
-
-    pub fn remove(&self, t: &T) -> bool {
-        unimplemented!();
-    }
+#[derive(Debug)]
+enum Child<S, T: Sized + Debug> {
+    Inner(Box<Node<S, T>>),
+    Leaf(S, Vec<T>)
 }
 
 #[derive(Debug, PartialEq, Eq, Hash, Clone, Copy)]
